3366e385fe7499dbce819b84112f3c98d10d14bc
soomin-world <ojudve123@gmail.com>
2022-12-16 17:57:44 +0900

    first commit

---
+++ src/shared/Router.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "../pages/Main";
+import Detail from "../pages/Detail";
+import Posting from "../pages/Posting";
+import PostingEdit from "../pages/PostingEdit";
+import Login from "../pages/Login";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="detail/:id" element={<Detail />} />
+        <Route path="/posting" element={<Posting />} />
+        <Route path="/postingEdit/:id" element={<PostingEdit />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
