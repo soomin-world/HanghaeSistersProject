4f6af1486db803c3f8bd9eb832d0f23660c7a603
soomin-world <ojudve123@gmail.com>
2022-12-21 02:02:20 +0900

    footer,header 수정및 배너 크기변경

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,19 +1,25 @@
 import { Carousel } from "antd";
+import styled from "styled-components";
 
 function Banner() {
   return (
-    <Carousel autoplay>
-      <div>
-        <img src="/image/banner1.jpg" alt="" />
-      </div>
-      <div>
-        <img src="/image/banner2.jpg" alt="" />
-      </div>
-      <div>
-        <img src="/image/banner5.jpg" alt="" />
-      </div>
-    </Carousel>
+    <STCarousel>
+      <Carousel autoplay>
+        <div>
+          <img src="/image/banner 1.jpg" alt="" />
+        </div>
+        <div>
+          <img src="/image/banner 2.jpg" alt="" />
+        </div>
+        <div>
+          <img src="/image/banner5.jpg" alt="" />
+        </div>
+      </Carousel>
+    </STCarousel>
   );
 }
+const STCarousel = styled.div`
+  justify-content: center;
+`;
 
 export default Banner;
