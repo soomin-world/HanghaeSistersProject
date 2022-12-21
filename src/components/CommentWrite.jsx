f521ea9d8bb61ea1bfa06b0b9dfabe51699e73cf
thdud <thgml1444@naver.com>
2022-12-21 17:34:26 +0900

    login update

--- src/components/CommentWrite.jsx
+++
@@ -1,6 +0,0 @@
-<ComWrite_box>
-<div>
-  <button>댓글작성</button>
-</div>
-
-</ComWrite_box>
--- src/redux/modules/userSlice.js
+++ src/redux/modules/userSlice.js
@@ -10,12 +10,15 @@ const initialState = {
   // user안에 리듀서가 관리할 데이터가 들어가고
   // isLoading - 서버와의 통신결과
   // error - 에러객체 저장
+  // 유저의 상태에 따라 추가 가능?
   user: [],
+  userCheck: [],
+  userSignup: null,
   isLoading: false,
   error: null,
 };
 
-// --------------------------------------------------Diary 미들웨어
+// ------------------------------------------------- 미들웨어
 // 중복체크 ( 유저 데이터 보내기, 결과 받기 )
 export const __userCheck = createAsyncThunk(
   "userCheck",
@@ -26,15 +29,7 @@ export const __userCheck = createAsyncThunk(
         `/api/user/idcheck?username=${payload}`
       );
       console.log("중복확인: ", data);
-
-      // 실제 서버통신 url
-      // const data = await axios.post("https://------/api/user/idcheck", payload)
-      // 중복확인에서 할 일 => 서버에서 status, msg출력해줌 => code에 따라서 메세지출력하기
-
-      // 여기서 결과가 출력되면 리듀서, 프론트 단에도 적용,
-      // 비동기라... 응답 받는 즉시 앞단에 출력?...
-      // dispatch끝난 다음 코드(console.)가 실행돼있어서.
-      return thunkAPI.fulfillWithValue(data, payload);
+      return thunkAPI.fulfillWithValue(data);
     } catch (err) {
       return thunkAPI.rejectWithValue(err);
     }
@@ -61,29 +56,13 @@ export const __loginUser = createAsyncThunk(
   "loginUser",
   async (payload, thunkAPI) => {
     try {
-      // 서버에 통신을 보낸다.
-      // payload확인
+
       console.log(payload);
-
-      // payload보내는 부분 체크
-      // const {data} = await axios.post('https://reqres.in/api/login',{ })
-      // 실제 URL
-      // const {data} = await axios.post('https://--------//api/user/login',{
-      //   "email": "eve.holt@reqres.in",
-      //   "password": "cityslicka" })
       const { data } = await instance.post("/api/user/login", payload);
-      console.log("로그인유저:", data);
-
-      // 로그인 성공하면 서버에서 토큰, code:200 줌.
-      // api호출 성공하면 token값 받아옴(경로확인), 헤더 저장
-      // data는 axios에서 기본적으로 결과값들이 들어있는 프로퍼티
-      let token = instance.defaults.headers.common["Authorization"];
-      // const token = data.token;
-      console.log(token);
-      // 쿠키에 is_login으로 토큰 저장 => 프론트가서 로그인 유지
-      // 토큰은 쿠키에 저장했으니, 스토어로 안감. 굳이.뭐하러.
-      // axios연결 이상 없는데, 리듀서로 가지 않으면 -> config에 연결확인해보기!
-      setCookie("is_login", token);
+      // let token = instance.defaults.headers.common["Authorization"];
+      // // const token = data.token;
+      // console.log(token);
+      // setCookie("is_login", token);
       return thunkAPI.fulfillWithValue(data, payload);
     } catch (err) {
       console.log(err);
@@ -119,8 +98,8 @@ export const userSlice = createSlice({
       // action으로 받아온 객체를 store에 있는 값에 넣어준다
       console.log("action-서버값", action);
       state.isLoading = false;
-      // status코드를 넘겨 => 프론트 단에서 state변경해
-      state.user = action.payload;
+      // 서버에서 받아오는 값을 저장할 공간이 겹쳤음 -> 새로 check로 만들어줌 
+      state.userCheck= action.payload;
     },
     [__userCheck.rejected]: (state, action) => {
       state.isLoading = false;
@@ -138,7 +117,7 @@ export const userSlice = createSlice({
       // action으로 받아온 객체를 store에 있는 값에 넣어준다
       console.log("action-서버값", action);
       state.isLoading = false;
-      state.user = action.payload;
+      state.userSignup = action.payload;
     },
     [__signUpUser.rejected]: (state, action) => {
       state.isLoading = false;
@@ -157,15 +136,8 @@ export const userSlice = createSlice({
       // 토큰은 쿠키에 저장했으니까 안해줘도 될거같고
       // 유저 데이터랑, 성공메세지(알림띄움용) 보내주면 될듯.
       console.log("action-서버값", action);
-      const username = action.meta.email;
-      console.log(username);
-
-      // store에 들어있는 값은 무엇? 로그인 한 유저의 정보만 있는건가?
-      // store는 모든 유저의 데이터가 들어있나??? 아닌것 같음.
-      // 리덕스는 새로고침하면 정보가 사라지니까
-      // 토큰도 스토어에 저장하면 안됨. 그렇네.
-      // 그렇다면. 로그인 할때 store값은 별로 중요하지 않다
-      state.user = { username };
+      console.log()
+      state.user = action.payload;
     },
     [__loginUser.rejected]: (state, action) => {
       state.isLoading = false;
